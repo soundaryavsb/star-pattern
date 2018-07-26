8bf0042e99ea1932953dd8ccce1adc6328247ff2
soundaryavsb <31506228+soundaryavsb@users.noreply.github.com>
2018-07-26 06:36:22 +0530

    Create HollowSquarePattern.c

---
+++ HollowSquarePattern.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+	int i,j,n;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			if(i==1||j==1||j==n||i==n)
+			{
+				printf("*");
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
